d588315b0d2ed8cd31393f6243b72ef41da8c210
mayowa-kalejaiye <kalejaiyemayowa3@gmail.com>
2025-10-06 21:10:48 +0100

    refactor: remove DebugAuthState component from Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,16 +8,13 @@ import dynamic from "next/dynamic";
 import { ChartLineInteractive } from '../components/ChartLineInteractive';
 import Testimonial2 from '../components/Testimonial2';
 import PricingSection from '../components/PricingSection';
-import DebugAuthState from '../components/DebugAuthState';
 import Footer2 from '../components/Footer2';
 // Dynamically import VerticalTabsDemo for client-side rendering
 const VerticalTabsDemo = dynamic(() => import("../components/VerticalTabsDemo").then(mod => mod.VerticalTabsDemo), { ssr: false });
 
-
 export default function Home() {
   return (
     <>
-      <DebugAuthState />
       <Hero3 />
       <HowItWorks />
       <section className="flex flex-col items-center mt-12">
